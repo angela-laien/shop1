ae6b533c40558a3749de8c8a6c9c1ece27be5668
angela-laien <laienxie@gmail.com>
2021-03-13 21:11:10 -0800

    added Mongo DB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+    users: [
+        {
+            name:'Angela',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name:'Lily',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+    ],
     products:[
         {
             _id: "1",
